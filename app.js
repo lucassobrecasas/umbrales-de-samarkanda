254a2d0d73539660a8323ab587100ffaa6a06738
lucassobrecasas <123843773+lucassobrecasas@users.noreply.github.com>
2024-10-25 00:37:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 
 
-window.addEventListener('scroll', function() {
-  let header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
-});
+
 
 
 window.onscroll = function() {myFunction()};
